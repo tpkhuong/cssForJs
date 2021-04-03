2cbf793291fb10b674b8a7615be5fa6aeed850ee
Toan <tpkhuong@gmail.com>
2021-04-03 06:27:57 -0400

    char creator Appjs

---
+++ moduleTwo/characterCreator/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import BottomScreen from "./components/BottomScreen";
+
+function App() {
+  return (
+    <>
+      <BottomScreen />
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
