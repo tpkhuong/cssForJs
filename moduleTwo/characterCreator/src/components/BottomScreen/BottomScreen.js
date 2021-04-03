c0934d024b228c3579940c8e42abda8ebede943b
Toan <tpkhuong@gmail.com>
2021-04-03 06:28:38 -0400

    BottomScreenjs char creator

---
+++ moduleTwo/characterCreator/src/components/BottomScreen/BottomScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./BottomScreen.module.css";
+
+function BottomScreen() {
+  return <article className={styles.bottomScreen}>Hello World!!!</article>;
+}
+
+export default BottomScreen;
