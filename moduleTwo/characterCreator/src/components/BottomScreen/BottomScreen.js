129294429695350130f37a5448f0c0c1511066c6
Toan <tpkhuong@gmail.com>
2021-04-12 06:54:33 -0400

    small changes

--- moduleTwo/characterCreator/src/components/BottomScreen/BottomScreen.js
+++ moduleTwo/characterCreator/src/components/BottomScreen/BottomScreen.js
@@ -3,6 +3,7 @@ import React from "react";
 import styles from "./BottomScreen.module.css";
 
 function BottomScreen() {
+  <button></button>;
   return <article className={styles.bottomScreen}>Hello World!!!</article>;
 }
 
