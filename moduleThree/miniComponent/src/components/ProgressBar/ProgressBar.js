7fabb10f7672ede1bd95c9891fe1ebea40f3cc01
Toan <tpkhuong@gmail.com>
2021-04-16 06:28:11 -0400

    progress bar is showing

--- moduleThree/miniComponent/src/components/ProgressBar/ProgressBar.js
+++ moduleThree/miniComponent/src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,35 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+
+var SIZES = {
+  small: {
+    "--padding": "2px",
+  },
+  medium: {
+    "--padding": "4px",
+  },
+  large: {
+    "--padding": "6px"
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  var styles = SIZES[size];
+  return (
+    <>
+    <LabelWrapper for="file">{ size }</LabelWrapper>
+    <Progress styles={styles} id="file" aria-valuemin="0" aria-valuemax="100" aria-valuenow={`${value}`} ></Progress>;
+    </>)
 };
+
+var Progress = styled.progress`
+ padding: var(--padding) 0;
+ border: 2px solid black;
+`
+
+var LabelWrapper = styled.label`
+  padding-inline-end: 8px;
+`
 
 export default ProgressBar;
