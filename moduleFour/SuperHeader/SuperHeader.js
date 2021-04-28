5474572554e52f3d25423a53dba7befa8e13034f
Toan <tpkhuong@gmail.com>
2021-04-28 11:21:45 -0400

    different approach

---
+++ moduleFour/SuperHeader/SuperHeader.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+import { COLORS } from "../../constants";
+
+import SearchInput from "../SearchInput";
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+
+const SuperHeader = () => {
+  return (
+    <Wrapper>
+      <MarketingMessage>
+        Free shipping on domestic orders over $75!
+      </MarketingMessage>
+      <SearchInput />
+      <HelpLink href="/help">Help</HelpLink>
+      <UnstyledButton>
+        <Icon id="shopping-bag" strokeWidth={1} />
+      </UnstyledButton>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  font-size: 0.875rem;
+  color: ${COLORS.gray[300]};
+  background-color: ${COLORS.gray[900]};
+  padding: 16px 32px;
+`;
+
+const MarketingMessage = styled.span`
+  color: ${COLORS.white};
+`;
+
+const HelpLink = styled.a`
+  color: inherit;
+  text-decoration: none;
+  outline-offset: 2px;
+  margin-inline: 16px;
+
+  &:not(:focus-visible) {
+    outline: none;
+  }
+`;
+
+export default SuperHeader;
