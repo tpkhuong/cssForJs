40b008fd26353c2e2ac41fcaaa792b6d95108f5d
Toan <tpkhuong@gmail.com>
2021-04-30 07:26:58 -0400

    tracking modulefour showindex  dir

---
+++ moduleFour/ShoeIndex/ShoeIndex.js
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import { WEIGHTS } from "../../constants";
+
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import Spacer from "../Spacer";
+import ShoeSidebar from "../ShoeSidebar";
+import ShoeGrid from "../ShoeGrid";
+
+const ShoeIndex = ({ sortId, setSortId }) => {
+  return (
+    <Wrapper>
+      <LeftColumn>
+        <Breadcrumbs>
+          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+        </Breadcrumbs>
+        <Spacer size={42} />
+        <ShoeSidebar />
+      </LeftColumn>
+      <MainColumn>
+        <Header>
+          <Title>Running</Title>
+          <Select
+            label="Sort"
+            value={sortId}
+            onChange={(ev) => setSortId(ev.target.value)}
+          >
+            <option value="newest">Newest Releases</option>
+            <option value="price">Price</option>
+          </Select>
+        </Header>
+        <Spacer size={34} />
+        <ShoeGrid />
+      </MainColumn>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const LeftColumn = styled.div``;
+
+const MainColumn = styled.div`
+  margin-inline-start: auto;
+  flex: 0 1 1200px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  align-items: baseline;
+`;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: ${WEIGHTS.medium};
+`;
+
+export default ShoeIndex;
