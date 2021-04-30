a7ae7228e687169f6ec072c988d507527c290a07
Toan <tpkhuong@gmail.com>
2021-04-30 07:26:18 -0400

    tracking modulefour breadcrumbs dir

---
+++ moduleFour/Breadcrumbs/Breadcrumbs.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import { COLORS } from "../../constants";
+
+const Breadcrumbs = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+Breadcrumbs.Crumb = ({ href, children, delegated }) => {
+  return (
+    <CrumbWrapper>
+      <CrumbLink href={href} {...delegated}>
+        {children}
+      </CrumbLink>
+    </CrumbWrapper>
+  );
+};
+
+const CrumbWrapper = styled.div`
+  margin-block-start: auto;
+  &:not(:first-of-type) {
+    margin-left: 8px;
+
+    &::before {
+      content: "/";
+      margin-right: 8px;
+      color: ${COLORS.gray[300]};
+    }
+  }
+`;
+
+const CrumbLink = styled.a`
+  color: ${COLORS.gray[700]};
+  text-decoration: none;
+
+  &:hover {
+    color: ${COLORS.gray[900]};
+  }
+`;
+
+const Wrapper = styled.nav`
+  display: flex;
+  font-size: 0.875rem;
+  height: 34px;
+`;
+export default Breadcrumbs;
