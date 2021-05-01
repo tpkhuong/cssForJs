1a9e671cbf1478f288fe0c2d4927c5dac8455522
Toan <tpkhuong@gmail.com>
2021-05-01 07:04:10 -0400

    modulefour workshop shoegrid dir

---
+++ moduleFour/ShoeGrid/ShoeGrid.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import SHOES from "../../data";
+import ShoeCard from "../ShoeCard";
+
+const ShoeGrid = () => {
+  return (
+    <Wrapper>
+      {SHOES.map((shoe) => (
+        <ShoeCard key={shoe.slug} {...shoe} />
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  gap: 28px;
+  & > * {
+    flex: 1 1 250px;
+  }
+`;
+
+export default ShoeGrid;
