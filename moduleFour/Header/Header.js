123333cbbc968299b4fd7f1e50e60be8e753cfe3
Toan <tpkhuong@gmail.com>
2021-05-02 06:43:31 -0400

    changes to moduleFour Header.js

--- moduleFour/Header/Header.js
+++ moduleFour/Header/Header.js
@@ -56,3 +56,71 @@ const NavLink = styled.a`
 `;
 
 export default Header;
+
+/* josh soluion */
+
+const Header = () => {
+  // Our site features two visual headers, but they should be
+  // grouped semantically as a single header.
+  return (
+    <header>
+      <SuperHeader />
+      <MainHeader>
+        <Side>
+          <Logo />
+        </Side>
+        <Side>
+          <Nav>
+            <NavLink href="/sale">Sale</NavLink>
+            <NavLink href="/new">New&nbsp;Releases</NavLink>
+            <NavLink href="/men">Men</NavLink>
+            <NavLink href="/women">Women</NavLink>
+            <NavLink href="/kids">Kids</NavLink>
+            <NavLink href="/collections">Collections</NavLink>
+          </Nav>
+        </Side>
+      </MainHeader>
+    </header>
+  );
+};
+
+const MainHeader = styled.div`
+  /* padding: 20px 32px;
+  padding-inline-end: 96px; */
+  /* using padding to center */
+  padding: 18px 32px;
+  /* using padding to center */
+  border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  height: 72px;
+  align-items: baseline;
+`;
+
+const Side = styled.div`
+  flex: 1;
+`;
+
+const Nav = styled.nav`
+  /* margin-inline: auto; */
+  display: flex;
+  gap: 48px;
+  margin: 0px 48px;
+  /* flex: 0 1 700px;
+  & > * + * {
+    margin-inline-start: auto;
+  } */
+`;
+
+const NavLink = styled.a`
+  font-size: 1.125rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+  font-weight: ${WEIGHTS.medium};
+
+  &:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`;
+
+/* josh soluion */
