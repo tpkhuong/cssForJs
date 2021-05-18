26a9474d15926b40a59884792ca56e3a3ec71c10
Toan <tpkhuong@gmail.com>
2021-05-18 06:46:10 -0400

    working on exercise 2 module five workshop constants js

--- moduleFive/soleAndAnklePartTwo/ourCopy/src/constants.js
+++ moduleFive/soleAndAnklePartTwo/ourCopy/src/constants.js
@@ -1,14 +1,27 @@
 export const COLORS = {
-  white: 'hsl(0deg 0% 100%)',
+  white: "hsl(0deg 0% 100%)",
   gray: {
-    100: 'hsl(185deg 5% 95%)',
-    300: 'hsl(190deg 5% 80%)',
-    500: 'hsl(196deg 4% 60%)',
-    700: 'hsl(220deg 5% 40%)',
-    900: 'hsl(220deg 3% 20%)',
+    100: "hsl(185deg 5% 95%)",
+    300: "hsl(190deg 5% 80%)",
+    500: "hsl(196deg 4% 60%)",
+    700: "hsl(220deg 5% 40%)",
+    900: "hsl(220deg 3% 20%)",
   },
-  primary: 'hsl(340deg 65% 47%)',
-  secondary: 'hsl(240deg 60% 63%)',
+  primary: "hsl(340deg 65% 47%)",
+  secondary: "hsl(240deg 60% 63%)",
+  black: `hsl(0, 0%,0%)`,
+};
+
+export const BREAKPOINTS = {
+  phoneMin: 37.5,
+  tabletMin: 59.375,
+  laptopMin: 81.25,
+};
+
+export const QUERIES = {
+  phoneAndUp: `(min-width: ${BREAKPOINTS.phoneMin}rem)`,
+  tabletAndUp: `(min-width: ${BREAKPOINTS.tabletMin}rem)`,
+  laptopAndUp: `(min-width: ${BREAKPOINTS.laptopMin}rem)`,
 };
 
 export const WEIGHTS = {
