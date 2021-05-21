f784498ac984b2a79f745fb3dcdbfd114b5177cd
Toan <tpkhuong@gmail.com>
2021-05-21 06:32:54 -0400

    breadcrumbs exercise complete shoeindex  js

--- moduleFive/soleAndAnklePartTwo/ourCopy/src/components/ShoeIndex/ShoeIndex.js
+++ moduleFive/soleAndAnklePartTwo/ourCopy/src/components/ShoeIndex/ShoeIndex.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, QUERIES } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import Spacer from "../Spacer";
+import ShoeSidebar from "../ShoeSidebar";
+import ShoeGrid from "../ShoeGrid";
 
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
       <MainColumn>
+        <DisplayNone>
+          <Breadcrumbs>
+            <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+          </Breadcrumbs>
+        </DisplayNone>
         <Header>
           <Title>Running</Title>
           <Select
@@ -31,9 +38,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
         <Breadcrumbs>
           <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
           <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
         </Breadcrumbs>
         <Spacer size={42} />
         <ShoeSidebar />
@@ -50,7 +55,18 @@ const Wrapper = styled.div`
 `;
 
 const LeftColumn = styled.div`
-  flex-basis: 248px;
+  display: none;
+  @media ${QUERIES.tabletAndUp} {
+    flex-basis: 248px;
+    display: revert;
+  }
+`;
+
+const DisplayNone = styled.span`
+  display: revert;
+  @media ${QUERIES.tabletAndUp} {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
