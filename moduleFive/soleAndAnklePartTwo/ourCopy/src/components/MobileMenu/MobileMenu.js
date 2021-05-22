957253cc9a94caa5eaf11050fc431619793e8edf
Toan <tpkhuong@gmail.com>
2021-05-22 06:42:47 -0400

    working on mobile layout mobilemenu js

--- moduleFive/soleAndAnklePartTwo/ourCopy/src/components/MobileMenu/MobileMenu.js
+++ moduleFive/soleAndAnklePartTwo/ourCopy/src/components/MobileMenu/MobileMenu.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   if (!isOpen) {
@@ -15,23 +15,40 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
+    <MobileMenuWrapper>
       <button onClick={onDismiss}>Dismiss menu</button>
       <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
+        <MobileNavlink href="/sale">Sale</MobileNavlink>
+        <MobileNavlink href="/new">New&nbsp;Releases</MobileNavlink>
+        <MobileNavlink href="/men">Men</MobileNavlink>
+        <MobileNavlink href="/women">Women</MobileNavlink>
+        <MobileNavlink href="/kids">Kids</MobileNavlink>
+        <MobileNavlink href="/collections">Collections</MobileNavlink>
       </nav>
       <footer>
         <a href="/terms">Terms and Conditions</a>
         <a href="/privacy">Privacy Policy</a>
         <a href="/contact">Contact Us</a>
       </footer>
-    </div>
+    </MobileMenuWrapper>
   );
 };
+
+const MobileMenuWrapper = styled.div`
+  border: 1px solid ${COLORS.red};
+`;
+
+const MobileNav = styled.nav``;
+const MobileDismissButton = styled.button``;
+
+const MobileNavlink = styled.a`
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const MobileMenuFooter = styled.footer``;
 
 export default MobileMenu;
