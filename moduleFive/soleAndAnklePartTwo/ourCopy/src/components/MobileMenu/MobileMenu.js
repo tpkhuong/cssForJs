2e62e38537a3b2a9866c2384dc08514dbc47f637
Toan <tpkhuong@gmail.com>
2021-05-23 06:49:32 -0400

    working on mobile layout styles

--- moduleFive/soleAndAnklePartTwo/ourCopy/src/components/MobileMenu/MobileMenu.js
+++ moduleFive/soleAndAnklePartTwo/ourCopy/src/components/MobileMenu/MobileMenu.js
@@ -13,37 +13,80 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   if (!isOpen) {
     return null;
   }
-
+  // add accessibility attributes for modal
   return (
-    <MobileMenuWrapper>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <MobileNavlink href="/sale">Sale</MobileNavlink>
-        <MobileNavlink href="/new">New&nbsp;Releases</MobileNavlink>
-        <MobileNavlink href="/men">Men</MobileNavlink>
-        <MobileNavlink href="/women">Women</MobileNavlink>
-        <MobileNavlink href="/kids">Kids</MobileNavlink>
-        <MobileNavlink href="/collections">Collections</MobileNavlink>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </MobileMenuWrapper>
+    <ModalBackdrop>
+      <MobileMenuWrapper
+        aria-modal="true"
+        role="modal"
+        id="dialog1"
+        aria-labelledby="dialog1-label"
+      >
+        <ModalTitle id="dialog1-label">Navigation Menu</ModalTitle>
+        {/* <button onClick={onDismiss}>Dismiss menu</button> */}
+        <UnstyledButton>
+          <Icon onClick={onDismiss} id="close" strokeWidth={1} />
+        </UnstyledButton>
+        <MobileNav>
+          <MobileNavlink href="/sale">Sale</MobileNavlink>
+          <MobileNavlink href="/new">New&nbsp;Releases</MobileNavlink>
+          <MobileNavlink href="/men">Men</MobileNavlink>
+          <MobileNavlink href="/women">Women</MobileNavlink>
+          <MobileNavlink href="/kids">Kids</MobileNavlink>
+          <MobileNavlink href="/collections">Collections</MobileNavlink>
+        </MobileNav>
+        <footer>
+          <a href="/terms">Terms and Conditions</a>
+          <a href="/privacy">Privacy Policy</a>
+          <a href="/contact">Contact Us</a>
+        </footer>
+      </MobileMenuWrapper>
+    </ModalBackdrop>
   );
 };
 
-const MobileMenuWrapper = styled.div`
-  border: 1px solid ${COLORS.red};
+const ModalBackdrop = styled.div`
+  position: fixed;
+  background-color: hsla(0, 0%, 20%, 0.9);
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
 `;
 
-const MobileNav = styled.nav``;
+const ModalTitle = styled.span`
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+`;
+
+const MobileMenuWrapper = styled.div`
+  position: fixed;
+  border: 1px solid ${COLORS.red};
+  /* width: 100%; */
+  /* z-index: 1; */
+  background-color: ${COLORS.white};
+  /* add background color and border around links */
+`;
+
+const MobileNav = styled.nav`
+  text-align: center;
+  font-weight: ${WEIGHTS.bold};
+`;
 const MobileDismissButton = styled.button``;
 
 const MobileNavlink = styled.a`
   text-decoration: none;
   color: ${COLORS.gray[900]};
+  display: block;
+  padding: 16px;
+  /* border: 1px solid ${COLORS.gray[700]}; */
   &:hover {
     text-decoration: underline;
   }
