a7f6a4f187c04dd61c35848feb4254d0f110108c
Toan <tpkhuong@gmail.com>
2021-06-01 06:43:58 -0400

    exercise 6 searchinput js complete

--- moduleFive/soleAndAnklePartTwo/ourCopy/src/components/SearchInput/SearchInput.js
+++ moduleFive/soleAndAnklePartTwo/ourCopy/src/components/SearchInput/SearchInput.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import VisuallyHidden from '../VisuallyHidden';
-import Icon from '../Icon';
+import VisuallyHidden from "../VisuallyHidden";
+import Icon from "../Icon";
 
 const SearchInput = ({ label, ...delegated }) => {
   return (
@@ -23,14 +23,17 @@ const Label = styled.label`
 const Input = styled.input`
   border: none;
   background: transparent;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+  border-bottom: 1px solid var(--color-gray-300);
+  /* border-bottom: 1px solid ${COLORS.gray[300]}; */
   padding-left: 24px;
   font-size: 0.875rem;
-  color: ${COLORS.gray[100]};
+  color: var(--color-gray-100);
+  /* color: ${COLORS.gray[100]}; */
   outline-offset: 4px;
 
   &::placeholder {
-    color: ${COLORS.gray[500]};
+    color: var(--color-gray-500);
+    /* color: ${COLORS.gray[500]}; */
   }
 `;
 
