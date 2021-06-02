9a33564978264a814f5f835238d1ecff0351d32b
Toan <tpkhuong@gmail.com>
2021-06-02 06:40:51 -0400

    exercise 4 ShoeIndex js josh solution

--- moduleFive/soleAndAnklePartTwo/ourCopy/src/components/ShoeIndex/ShoeIndex.js
+++ moduleFive/soleAndAnklePartTwo/ourCopy/src/components/ShoeIndex/ShoeIndex.js
@@ -85,4 +85,131 @@ const Title = styled.h2`
   /* font-weight: ${WEIGHTS.medium}; */
 `;
 
+/***** josh's solution *****/
+
+import React from "react";
+import styled from "styled-components/macro";
+
+import { WEIGHTS, QUERIES } from "../../constants";
+
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import Spacer from "../Spacer";
+import ShoeSidebar from "../ShoeSidebar";
+import ShoeGrid from "../ShoeGrid";
+
+const ShoeBreadcrumbs = () => {
+  return (
+    <Breadcrumbs>
+      <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+    </Breadcrumbs>
+  );
+};
+
+const ShoeIndex = ({ sortId, setSortId }) => {
+  return (
+    <Wrapper>
+      <MainColumn>
+        <DisplayNone>
+          <Breadcrumbs>
+            <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+          </Breadcrumbs>
+        </DisplayNone>
+        <Header>
+          <div>
+            <MobileBreadcrumbs>
+              <ShoeBreadcrumbs />
+            </MobileBreadcrumbs>
+            <Title>Running</Title>
+          </div>
+          <SortFilterWrapper>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </SortFilterWrapper>
+        </Header>
+        <Spacer size={32} />
+        <ShoeGrid />
+      </MainColumn>
+      <LeftColumn>
+        <DesktopBreadcrumbs>
+          <ShoeBreadcrumbs />
+        </DesktopBreadcrumbs>
+        <Spacer size={42} />
+        <ShoeSidebar />
+      </LeftColumn>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: baseline;
+  gap: 32px;
+`;
+
+const LeftColumn = styled.div`
+  flex-basis: 248px;
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
+
+const DisplayNone = styled.span`
+  display: revert;
+  @media ${QUERIES.tabletAndUp} {
+    display: none;
+  }
+`;
+
+const MainColumn = styled.div`
+  flex: 1;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  @media ${QUERIES.tabletAndSmaller} {
+    align-items: flex-end;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: var(--font-weight-600);
+  /* font-weight: ${WEIGHTS.medium}; */
+`;
+
+const DesktopBreadcrumbs = styled.div`
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
+
+const MobileBreadcrumbs = styled.div`
+  display: none;
+  @media ${QUERIES.tabletAndSmaller} {
+    display: revert;
+  }
+`;
+
+const SortFilterWrapper = styled.div`
+  @media ${QUERIES.phoneAndSmaller} {
+    display: none;
+  }
+`;
+
+/***** josh's solution *****/
+
 export default ShoeIndex;
