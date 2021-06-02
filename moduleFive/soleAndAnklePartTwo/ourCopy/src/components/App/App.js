997f5bd38fcfc632eedddbc44820189b1109d2ad
Toan <tpkhuong@gmail.com>
2021-06-02 06:39:51 -0400

    exercise 4 App js josh solution

--- moduleFive/soleAndAnklePartTwo/ourCopy/src/components/App/App.js
+++ moduleFive/soleAndAnklePartTwo/ourCopy/src/components/App/App.js
@@ -3,6 +3,7 @@ import styled from "styled-components/macro";
 
 import Header from "../Header";
 import ShoeIndex from "../ShoeIndex";
+import { QUERIES } from "../../constants";
 
 const App = () => {
   const [sortId, setSortId] = React.useState("newest");
@@ -20,5 +21,39 @@ const App = () => {
 const Main = styled.main`
   padding: 64px 32px;
 `;
+
+/***** josh's solution *****/
+
+import React from "react";
+import styled from "styled-components/macro";
+
+import Header from "../Header";
+import ShoeIndex from "../ShoeIndex";
+
+const App = () => {
+  const [sortId, setSortId] = React.useState("newest");
+
+  return (
+    <>
+      <Header />
+      <Main>
+        <ShoeIndex sortId={sortId} setSortId={setSortId} />
+      </Main>
+    </>
+  );
+};
+
+const Main = styled.main`
+  padding: 64px 32px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    padding: 48px 32px;
+  }
+  @media ${QUERIES.phoneAndSmaller} {
+    padding: 48px 16px;
+  }
+`;
+
+/***** josh's solution *****/
 
 export default App;
