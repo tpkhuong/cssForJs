b1e9a0aab71e172450a6b48ba9b1282288a5958e
Toan <tpkhuong@gmail.com>
2021-06-03 06:33:31 -0400

    exercise 6 josh solution constants js

--- moduleFive/soleAndAnklePartTwo/ourCopy/src/constants.js
+++ moduleFive/soleAndAnklePartTwo/ourCopy/src/constants.js
@@ -45,3 +45,57 @@ export const QUERIES = {
   laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptop / 16}rem)`,
 };
 /***** josh solution *****/
+
+/***** josh solution: using CSS variables *****/
+
+export const COLORS = {
+  white: "hsl(0deg 0% 100%)",
+  gray: {
+    100: "hsl(185deg 5% 95%)",
+    300: "hsl(190deg 5% 80%)",
+    500: "hsl(196deg 4% 60%)",
+    700: "hsl(220deg 5% 40%)",
+    900: "hsl(220deg 3% 20%)",
+  },
+  primary: "hsl(340deg 65% 47%)",
+  secondary: "hsl(240deg 60% 63%)",
+  black: `hsl(0, 0%,0%)`,
+  red: "hsl(0,100%,50%)",
+};
+
+// made changes to work with unique backdrop color in globalstyles
+export const COLORS = {
+  white: "0deg 0% 100%",
+  gray: {
+    100: "185deg 5% 95%",
+    300: "190deg 5% 80%",
+    500: "196deg 4% 60%",
+    700: "220deg 5% 40%",
+    900: "220deg 3% 20%",
+  },
+  primary: "hsl(340deg 65% 47%)",
+  secondary: "hsl(240deg 60% 63%)",
+  black: `hsl(0, 0%,0%)`,
+  red: "hsl(0,100%,50%)",
+};
+// made changes to work with unique backdrop color in globalstyles
+
+export const BREAKPOINTS = {
+  phoneMin: 37.5,
+  tabletMin: 59.375,
+  laptopMin: 81.25,
+};
+
+export const QUERIES = {
+  phoneAndUp: `(min-width: ${BREAKPOINTS.phoneMin}rem)`,
+  tabletAndUp: `(min-width: ${BREAKPOINTS.tabletMin}rem)`,
+  laptopAndUp: `(min-width: ${BREAKPOINTS.laptopMin}rem)`,
+};
+
+export const WEIGHTS = {
+  normal: 500,
+  medium: 600,
+  bold: 800,
+};
+
+/***** josh solution: using CSS variables *****/
