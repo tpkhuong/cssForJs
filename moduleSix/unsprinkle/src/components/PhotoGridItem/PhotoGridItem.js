85e386ba325039d4bc48c5112154b2180670b036
Toan <tpkhuong@gmail.com>
2021-06-24 06:30:58 -0400

    exercise 3 josh's solution

--- moduleSix/unsprinkle/src/components/PhotoGridItem/PhotoGridItem.js
+++ moduleSix/unsprinkle/src/components/PhotoGridItem/PhotoGridItem.js
@@ -58,75 +58,76 @@ export default PhotoGridItem;
 
 /* josh's solution */
 
-// import React from 'react';
-// import styled from 'styled-components/macro';
+import React from 'react';
+import styled from 'styled-components/macro';
 
-// const PhotoGridItem = ({ id, src, alt, tags }) => {
-//   return (
-//     <article>
-//       <Anchor href={`/photos/${id}`}>
-//         <picture>
-//           {/* order matters we want to most optimized file format. like a switch statement in js. the source will break once it finds something that works */}
-//           {/* avif before/ahead jpeg */}
-//           <source
-//             type="image/avif"
-//             // srcSet have to be camelCase in JSX. we had it as srcset
-//             srcSet={`
-//             ${src} 1x,
-//             ${src.replace('.jpg', '@2x.avif')} 2x
-//             ${src.replace('.jpg', '@3x.avif')} 3x
-//             `}
-//           />
-//           <source
-//             type="image/jpeg"
-//             // srcSet have to be camelCase in JSX. we had it as srcset
-//             srcSet={`
-//             ${src} 1x,
-//             ${src.replace('.jpg', '@2x.jpg')} 2x
-//             ${src.replace('.jpg', '@3x.jpg')} 3x
-//             `}
-//           />
-//           <Image src={src} />
-//         </picture>
-//       </Anchor>
-//       <Tags>
-//         {tags.map((tag) => (
-//           <Tag key={tag}>{tag}</Tag>
-//         ))}
-//       </Tags>
-//     </article>
-//   );
-// };
+const PhotoGridItem = ({ id, src, alt, tags }) => {
+  return (
+    <article>
+      <Anchor href={`/photos/${id}`}>
+        <picture>
+          {/* order matters we want to most optimized file format. like a switch statement in js. the source will break once it finds something that works */}
+          {/* avif before/ahead jpeg */}
+          <source
+            type="image/avif"
+            // srcSet have to be camelCase in JSX. we had it as srcset
+            srcSet={`
+            ${src} 1x,
+            ${src.replace('.jpg', '@2x.avif')} 2x
+            ${src.replace('.jpg', '@3x.avif')} 3x
+            `}
+          />
+          <source
+            type="image/jpeg"
+            // srcSet have to be camelCase in JSX. we had it as srcset
+            srcSet={`
+            ${src} 1x,
+            ${src.replace('.jpg', '@2x.jpg')} 2x
+            ${src.replace('.jpg', '@3x.jpg')} 3x
+            `}
+          />
+          {/* josh added alt={alt} to <Image/> */}
+          <Image src={src} />
+        </picture>
+      </Anchor>
+      <Tags>
+        {tags.map((tag) => (
+          <Tag key={tag}>{tag}</Tag>
+        ))}
+      </Tags>
+    </article>
+  );
+};
 
-// const Anchor = styled.a`
-//   text-decoration: none;
-//   color: inherit;
-//   outline-offset: 4px;
-// `;
+const Anchor = styled.a`
+  text-decoration: none;
+  color: inherit;
+  outline-offset: 4px;
+`;
 
-// const Image = styled.img`
-//   display: block;
-//   width: 100%;
-//   height: 300px;
-//   border-radius: 2px;
-//   margin-bottom: 8px;
-//   object-fit: cover;
-// `;
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 300px;
+  border-radius: 2px;
+  margin-bottom: 8px;
+  object-fit: cover;
+`;
 
-// const Tags = styled.ul`
-//   display: flex;
-//   flex-wrap: wrap;
-//   gap: 8px;
-// `;
+const Tags = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
 
-// const Tag = styled.li`
-//   padding: 4px 8px;
-//   background: var(--color-gray-300);
-//   font-size: 0.875rem;
-//   font-weight: 475;
-//   color: var(--color-gray-800);
-// `;
+const Tag = styled.li`
+  padding: 4px 8px;
+  background: var(--color-gray-300);
+  font-size: 0.875rem;
+  font-weight: 475;
+  color: var(--color-gray-800);
+`;
 
-// export default PhotoGridItem;
+export default PhotoGridItem;
 
 /* josh's solution */
