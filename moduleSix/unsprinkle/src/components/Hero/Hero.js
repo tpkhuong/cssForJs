38a4593eeeb27dda59ed4f4a1a231eb0e9bb3959
Toan <tpkhuong@gmail.com>
2021-07-10 07:05:10 -0400

    josh solution hero js

--- moduleSix/unsprinkle/src/components/Hero/Hero.js
+++ moduleSix/unsprinkle/src/components/Hero/Hero.js
@@ -27,8 +27,10 @@ const Hero = () => {
           src="/images/hero-img.jpg"
         />
       </picture>
+      
       <Swoop alt="" src="/swoop.svg" />
     </Wrapper>
+    
   );
 };
 
@@ -71,47 +73,71 @@ export default Hero;
 
 /* josh's solution */
 
-// import React from "react";
-// import styled from "styled-components/macro";
+import React from "react";
+import styled from "styled-components/macro";
 
-// const Hero = () => {
-//   return (
-//     <Wrapper>
-//       <HeroImage src="/images/hero-img.jpg" />
-//       <Swoop src="/swoop.svg" />
-//     </Wrapper>
-//   );
-// };
+const Hero = () => {
+  return (
+    <Wrapper>
+      <picture>
+        <source
+          type="image/avif"
+          srcset="
+          /moduleSix/unsprinkle/public/images/hero-img.avif 1x,
+          /moduleSix/unsprinkle/public/images/hero-img@2x.avif 2x,
+          /moduleSix/unsprinkle/public/images/hero-img@3x.avif 3x
+        "
+        />
+        <source
+          type="image/jpg"
+          srcset="
+          /moduleSix/unsprinkle/public/images/hero-img.jpg 1x,
+          /moduleSix/unsprinkle/public/images/hero-img@2x.jpg 2x,
+          /moduleSix/unsprinkle/public/images/hero-img@3x.jpg 3x
+        "
+        />
 
-// const Wrapper = styled.section`
-//   position: relative;
-//   height: 60vh;
-//   min-height: 400px;
-//   display: flex;
-//   justify-content: center;
-//   align-items: flex-end;
-//   background: hsl(0deg 0% 1%);
-// `;
+        <HeroImage
+          // provide context
+          alt="An example of the photos distributed through this site, (cat on black background)"
+          src="/images/hero-img.jpg"
+        />
+      </picture>
+      {/* swoop img is decorative */}
+      <Swoop alt="" src="/swoop.svg" />
+    </Wrapper>
+  );
+};
 
-// const HeroImage = styled.img`
-//   display: block;
-//   width: 500px;
-//   height: 500px;
-//   max-height: 100%;
-// `;
+const Wrapper = styled.section`
+  position: relative;
+  height: 60vh;
+  min-height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  background: hsl(0deg 0% 1%);
+`;
 
-// const Swoop = styled.img`
-//   position: absolute;
-//   left: 0;
-//   right: 0;
-//   /*
-//     Overhang by a couple px to prevent any pixel-rounding
-//     display issues
-//   */
-//   bottom: -2px;
-//   width: 100%;
-// `;
+const HeroImage = styled.img`
+  display: block;
+  width: 500px;
+  height: 500px;
+  max-height: 100%;
+`;
 
-// export default Hero;
+const Swoop = styled.img`
+  position: absolute;
+  left: 0;
+  right: 0;
+  /*
+    Overhang by a couple px to prevent any pixel-rounding
+    display issues
+  */
+  bottom: -2px;
+  width: 100%;
+`;
+
+export default Hero;
 
 /* josh's solution */
